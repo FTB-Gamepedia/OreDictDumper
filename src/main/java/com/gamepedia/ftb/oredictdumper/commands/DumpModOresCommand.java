7cba2ee0bdc7177ac0a39dff7b4abacf7ae503a1
elifoster <elifosterwy@gmail.com>
2017-05-28 14:47:09 -0700

    :arrow_up: Update wiki format for OreDict extension 3.0.0 Fix #5 for 1.8

--- src/main/java/com/gamepedia/ftb/oredictdumper/commands/DumpModOresCommand.java
+++ src/main/java/com/gamepedia/ftb/oredictdumper/commands/DumpModOresCommand.java
@@ -63,7 +63,7 @@ public class DumpModOresCommand implements ICommand {
         switch (format) {
             case "wiki": {
                 for (OreDictEntry entry : entries) {
-                    builder.append(String.format("%s!%s!%s!!\n", entry.tagName, entry
+                    builder.append(String.format("%s!%s!%s!\n", entry.tagName, entry
                       .displayName, abbreviation));
                 }
                 break;

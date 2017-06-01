28008953fced4d50a790c3733932a595720a2c69
elifoster <elifosterwy@gmail.com>
2017-05-31 20:15:54 -0700

    :package: Generally improve the design of the codebase New OreDumpCommandBase abstract class for DumpAllOresCommand and DumpModOresCommand to inherit from. It provides some abstract methods needed for getting basic usage information on the commands, so that the base class can handle all of the :meat_on_bone: of the command execution. New OreDictOutputFormat class for handling List<OreDictEntry> -> output text in a more concise, readable, and understandable way. It also allows for easily expanding/adding new output formats if we ever need to do that. No longer output a ChatComponentText with a translated string to the chat, but a ChatComponentTranslation with its style set to a new ChatStyleColored generic class. OreDictDumperMod.getEntries(String) is now OreDumpCommandBase#getEntries(String), which is a much more logical design. The only downfall (although perhaps it is not actually much of a downfall, since there is no reason everyone should be assumed to use our specific wiki format) is that the /dumpmodores command now requires the format argument instead of assuming wiki.

---
+++ src/main/java/com/gamepedia/ftb/oredictdumper/misc/OreDictOutputFormat.java
@@ -0,0 +1,72 @@
+package com.gamepedia.ftb.oredictdumper.misc;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+public abstract class OreDictOutputFormat {
+    private final String extension;
+
+    OreDictOutputFormat(String extension) {
+        this.extension = extension;
+    }
+
+    @Nonnull
+    public String getFileExtension() {
+        return extension;
+    }
+
+    @Nonnull
+    public abstract String parseEntries(List<OreDictEntry> entries);
+
+    public static class JSONOutputFormat extends OreDictOutputFormat {
+        public JSONOutputFormat() {
+            super("json");
+        }
+
+        @Nonnull
+        @Override
+        public String parseEntries(List<OreDictEntry> entries) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            return gson.toJson(entries);
+        }
+    }
+
+    public static class CSVOutputFormat extends OreDictOutputFormat {
+        public CSVOutputFormat() {
+            super("csv");
+        }
+
+        @Nonnull
+        @Override
+        public String parseEntries(List<OreDictEntry> entries) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("Tag,ItemName,Metadata,ModID\n");
+            for (OreDictEntry entry : entries) {
+                builder.append(String.format("%s,%s,%s,%s\n", entry.tagName, entry.displayName, entry.metadata, entry.modID));
+            }
+            return builder.toString();
+        }
+    }
+
+    public static class WikiOutputFormat extends OreDictOutputFormat {
+        private final String abbreviation;
+
+        public WikiOutputFormat(String abbreviation) {
+            super("txt");
+            this.abbreviation = abbreviation;
+        }
+
+        @Nonnull
+        @Override
+        public String parseEntries(List<OreDictEntry> entries) {
+            StringBuilder builder = new StringBuilder();
+            for (OreDictEntry entry : entries) {
+                builder.append(String.format("%s!%s!%s!\n", entry.tagName, entry.displayName, abbreviation));
+            }
+            return builder.toString();
+        }
+    }
+}

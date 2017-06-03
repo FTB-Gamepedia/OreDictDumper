27cc08aa2d8de8b34087f9a741ce4e4b35f47509
elifoster <elifosterwy@gmail.com>
2017-06-02 19:57:44 -0700

    :birthday: Bump version to 4.0.0

--- src/main/java/com/gamepedia/ftb/oredictdumper/OreDictDumperMod.java
+++ src/main/java/com/gamepedia/ftb/oredictdumper/OreDictDumperMod.java
@@ -2,22 +2,11 @@ package com.gamepedia.ftb.oredictdumper;
 
 import com.gamepedia.ftb.oredictdumper.commands.DumpAllOresCommand;
 import com.gamepedia.ftb.oredictdumper.commands.DumpModOresCommand;
-import com.gamepedia.ftb.oredictdumper.misc.OreDictEntry;
-import com.google.common.collect.ImmutableList;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.NonNullList;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.oredict.OreDictionary;
 
-import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.List;
-
-@Mod(modid = "oredictdumper", name = "OreDictDumper", version = "3.1.3", acceptedMinecraftVersions = "[1.11,1.11.2]")
+@Mod(modid = "oredictdumper", name = "OreDictDumper", version = "4.0.0", acceptedMinecraftVersions = "[1.11,1.11.2]")
 public class OreDictDumperMod {
     @Mod.EventHandler
     public void registerCommand(FMLPostInitializationEvent event) {

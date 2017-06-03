43e8dcf71309a575dbfffae1741d9f575cd62641
elifoster <elifosterwy@gmail.com>
2017-06-02 18:36:32 -0700

    :bug: Throw WrongUsageExceptions in the command execution code instead of silently returning Resolve #6 for 1.9

--- src/main/java/com/gamepedia/ftb/oredictdumper/commands/DumpAllOresCommand.java
+++ src/main/java/com/gamepedia/ftb/oredictdumper/commands/DumpAllOresCommand.java
@@ -7,8 +7,10 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.command.WrongUsageException;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
@@ -43,15 +45,19 @@ public class DumpAllOresCommand implements ICommand {
     }
 
     @Override
-    public void execute(MinecraftServer server, ICommandSender sender, String[] args) {
-        if (args.length != 1 || !sender.getEntityWorld().isRemote) {
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+        if (!sender.getEntityWorld().isRemote) {
             return;
+        }
+
+        if (args.length != 1) {
+            throw new WrongUsageException("commands.dumpallores.usage", Joiner.on(',').join(FORMATS));
         }
 
         String format = args[0].toLowerCase();
 
         if (!FORMATS.contains(format)) {
-            return;
+            throw new WrongUsageException("commands.dumpallores.usage", Joiner.on(',').join(FORMATS));
         }
 
         ArrayList<OreDictEntry> entries = OreDictDumperMod.getEntries(null);

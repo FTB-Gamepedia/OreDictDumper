beab2d3d6d6fd73f9788c36ef02788cda2e1b79e
elifoster <elifosterwy@gmail.com>
2017-06-02 18:15:12 -0700

    :birthday: Bump version to 2.2.0

--- src/main/java/com/gamepedia/ftb/oredictdumper/OreDictDumperMod.java
+++ src/main/java/com/gamepedia/ftb/oredictdumper/OreDictDumperMod.java
@@ -6,7 +6,7 @@ import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
-@Mod(modid = "oredictdumper", name = "OreDictDumper", version = "2.1.2")
+@Mod(modid = "oredictdumper", name = "OreDictDumper", version = "2.2.0")
 public class OreDictDumperMod {
     @Mod.EventHandler
     public void registerCommand(FMLPostInitializationEvent event) {

b4f6e2bbd3af635003ca0a2013662d8878e76275
elifoster <elifosterwy@gmail.com>
2017-12-11 15:24:04 -0800

    :birthday: Bump version to 5.0.3

--- src/main/java/com/gamepedia/ftb/oredictdumper/OreDictDumperMod.java
+++ src/main/java/com/gamepedia/ftb/oredictdumper/OreDictDumperMod.java
@@ -6,7 +6,7 @@ import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
-@Mod(modid = "oredictdumper", name = "OreDictDumper", version = "5.0.2", acceptedMinecraftVersions = "[1.12,1.12.2]", clientSideOnly = true)
+@Mod(modid = "oredictdumper", name = "OreDictDumper", version = "5.0.3", acceptedMinecraftVersions = "[1.12,1.12.2]", clientSideOnly = true)
 public class OreDictDumperMod {
     @Mod.EventHandler
     public void registerCommand(FMLPostInitializationEvent event) {

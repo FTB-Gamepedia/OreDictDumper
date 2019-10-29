e591e5910499979c6cdd6ea6d9628a10f221b8e5
elifoster <elifosterwy@gmail.com>
2019-10-29 11:59:35 -0700

    :gem: Change order of dumpmodores parameters <abbreviation> <modid> <format> is now <modid> <format> [abbreviation] abbreviation parameter is now not needed for csv and json, only wiki format. Optional parameters are now allowed by making getRequiredNumberOfArguments return the minimum number, not the exact value (check changed from != to <) Files from dumpmodores are now saved as modid rather than abbreviation (minecraft.txt instead of V.txt, for example)

--- src/main/java/com/gamepedia/ftb/oredictdumper/commands/OreDumpCommandBase.java
+++ src/main/java/com/gamepedia/ftb/oredictdumper/commands/OreDumpCommandBase.java
@@ -81,7 +81,7 @@ public abstract class OreDumpCommandBase implements ICommand {
             return;
         }
 
-        if (args.length != getRequiredNumberOfArguments()) {
+        if (args.length < getRequiredNumberOfArguments()) {
             throw new WrongUsageException(getUnlocalizedCommandUsage(), Joiner.on(',').join(getValidFormats()));
         }
 
